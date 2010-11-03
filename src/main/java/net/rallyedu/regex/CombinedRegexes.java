97c92bb68c350246272b8954580b228595a88601
Will Butler <will@rallydev.com>
2010-11-03 06:36:52 -0600

    renaming, using two examples

---
+++ src/main/java/net/rallyedu/regex/CombinedRegexes.java
@@ -0,0 +1,6 @@
+package net.rallyedu.regex;
+
+public class CombinedRegexes {
+    public static final String PHONE_NUMBER = "";
+    public static final String EMPHASIZED_TEXT = "";
+}
--- src/main/java/net/rallyedu/regex/ComplexRegexes.java
+++
@@ -1,7 +0,0 @@
-package net.rallyedu.regex;
-
-public class ComplexRegexes {
-    public static final String EMAIL_ADDRESS = "";
-    public static final String PHONE_NUMBER = "";
-    public static final String HEADINGS = "";
-}
