41286ec782ffb06c989f4e60b1cfa0b4029ea847
Will Butler <will@rallydev.com>
2010-11-02 21:48:57 -0600

    Removing more complex character classes

--- src/main/java/net/rallyedu/regex/CharacterClassRegexes.java
+++ src/main/java/net/rallyedu/regex/CharacterClassRegexes.java
@@ -4,7 +4,4 @@ public class CharacterClassRegexes {
     public static final String SIMPLE = "";
     public static final String NEGATION = "";
     public static final String RANGE = "";
-    public static final String UNION = "";
-    public static final String INTERSECTION = "";
-    public static final String SUBTRACTION = "";
 }
